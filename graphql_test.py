95fb3ebb3375d9c0b8bf75397aff77a4d014db73
Dan Stephenson <ispyhumanfly@gmail.com>
2023-10-02 14:54:13 -0400

    It's now working.

--- graphql_test.py
+++ graphql_test.py
@@ -1,30 +1,54 @@
 #!python
 
 """Testing connecting to Drone Deploy GraphQL API"""
-
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-endpoint = "https://dronedeploy.com/graphql"
+graphql_query = """
 
-token = os.getenv("DRONE_DEPLOY_API_KEY")
-
-query = """
-
-query {
-    viewer {
-        login
+{
+  projects {
+    edges {
+      node {
+        location {
+          lat
+          lng
+        }
+        name
+        plans {
+          edges {
+            node {
+              id
+              name
+              ... on MapPlanTemplate {
+                id
+                name
+                geometry {
+                  lat
+                  lng
+                }
+              }
+            }
+          }
+        }
+      }
     }
+  }
+  viewer {
+    id
+  }
 }
 """
 
-response = requests.post(endpoint, json={'query': query}, headers={'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}, timeout=10)
+response = requests.post(url="https://api.dronedeploy.com/graphql", json={"query": graphql_query}, headers={'Authorization': 'Bearer ' + os.getenv("DRONE_DEPLOY_API_KEY"), 'Content-Type': 'application/json'}, timeout=10)
 
 if response.status_code == 200:
     print("We succeeded")
+    print(response.text)
 else:
     print(response.status_code)
     print(response.text)
